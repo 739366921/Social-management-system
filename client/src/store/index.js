f3e1c9dcde20057349ce6e05c698af039c32b950
sheepboss <739366921@qq.com>
2020-08-05 03:12:45 +0800

    test

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,17 +1,17 @@
-// import Vue from "vue";
-// import Vuex from "vuex";
-// import * as getters from "./getters";
+import Vue from "vue";
+import Vuex from "vuex";
+import * as getters from "./getters";
 
-// Vue.use(Vuex);
+Vue.use(Vuex);
 
-// const state = {
-//   isAuthenticated: false, // 是否授权
-//   user: {}
-// };
+const state = {
+  isAuthenticated: false, // 是否授权
+  user: {}
+};
 
-// export default new Vuex.Store({
-//   state,
-//   getters,
-//   mutations: {},
-//   actions: {}
-// });
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations: {},
+  actions: {}
+});
